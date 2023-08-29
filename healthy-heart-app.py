42735739d7903b5d1fa396015aa2080fb03a93cb
Snupix031 <44869631+Snupix031@users.noreply.github.com>
2023-08-29 13:06:59 +0200

    Update healthy-heart-app.py

--- healthy-heart-app.py
+++ healthy-heart-app.py
@@ -112,14 +112,14 @@ st.write(slp)
 st.write(thall)
 st.write(restecg)
 
-#user_input=preprocess(sex,cp,exng, fbs, slp, thall )
-pred=preprocess(age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall)
+user_input=preprocess(sex,cp,exng, fbs, slp, thall )
+#pred=preprocess(age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall)
 
-st.success(pred)
+st.success(user_input)
 
 
 if st.button("Predict"):    
-  if pred[0] == 1:
+  if user_input == 1:
     st.error('Warning! You have high risk of getting a heart attack!')
     
   else:
