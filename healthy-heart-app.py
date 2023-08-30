f24d6bc2e9b0b922d86aad19971eea710bdbf1a9
Snupix031 <44869631+Snupix031@users.noreply.github.com>
2023-08-30 11:11:56 +0200

    Update healthy-heart-app.py

--- healthy-heart-app.py
+++ healthy-heart-app.py
@@ -106,6 +106,7 @@ thall=st.selectbox('Thalium Stress Result',range(0,4,1))
 
 pred=preprocess(age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall)
 
+st.write(pred[0])
 
 
 if st.button("Predict"):    
