78c5189bdd6a56278c1f904e9d332107646488fc
Snupix031 <44869631+Snupix031@users.noreply.github.com>
2023-09-03 16:59:05 +0200

    Update healthy-heart-app.py

--- healthy-heart-app.py
+++ healthy-heart-app.py
@@ -6,7 +6,7 @@ import pickle as pkl
 from sklearn.preprocessing import MinMaxScaler
 scal=MinMaxScaler()
 #Load the saved model
-model=pkl.load(open("final_model4.p","rb"))
+model=pkl.load(open("final_model3.p","rb"))
 
 
 
@@ -16,65 +16,65 @@ st.set_page_config(page_title="Healthy Heart App",page_icon="⚕️",layout="cen
 
 
 
-def preprocess(age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall):   
- 
-  
+def preprocess(age,sexP,cpP,trtbps,chol,fbsP,restecgP,thalachh,exngP,oldpeak,slpP,caa,thallP):   
+    
+    sex = 0
     # Pre-processing user input   
-    if sex=="male":
+    if sexP=="male":
         sex=1 
-    else: sex=0
     
-    
-    if cp=="Typical angina":
+    cp = 0
+    if cpP=="Typical angina":
         cp=0
-    elif cp=="Atypical angina":
+    elif cpP=="Atypical angina":
         cp=1
-    elif cp=="Non-anginal pain":
+    elif cpP=="Non-anginal pain":
         cp=2
-    elif cp=="Asymptomatic":
+    elif cpP=="Asymptomatic":
         cp=3
        
-    if exng=="Yes":
+    exng = 1
+    if exngP=="Yes":
         exng=1
-    elif exng=="No":
+    elif exngP=="No":
         exng=0
-    
-    if fbs=="Yes":
+
+    fbs = 1
+    if fbsP=="Yes":
         fbs=1
-    elif fbs=="No":
+    elif fbsP=="No":
         fbs=0
-   
-    if slp=="Upsloping: better heart rate with excercise(uncommon)":
+        
+    slp = 1
+    if slpP=="Upsloping: better heart rate with excercise(uncommon)":
         slp=0
-    elif slp=="Flatsloping: minimal change(typical healthy heart)":
+    elif slpP=="Flatsloping: minimal change(typical healthy heart)":
           slp=1
-    elif slp=="Downsloping: signs of unhealthy heart":
+    elif slpP=="Downsloping: signs of unhealthy heart":
         slp=2  
    
-    if thall==" null":
-        thall=0
-    elif thall=="fixed defect":
-        thall=1
-    elif thall=="normal":
-        thall=2
-    elif thall=="reversable defect":
-        thall=3
+    thall = 0
+    print('thallP ', thallP, type(thallP))
   
-    if restecg=="Nothing to note":
+    restecg = 0
+    if restecgP=="Nothing to note":
         restecg=0
-    elif restecg=="ST-T Wave abnormality":
+    elif restecgP=="ST-T Wave abnormality":
         restecg=1
-    elif restecg=="Possible or definite left ventricular hypertrophy":
+    elif restecgP=="Possible or definite left ventricular hypertrophy":
         restecg=2
      
-
-    user_input=[age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall]
-   
+    print('oldpeak ', type(oldpeak))
+    print('thalachh ', type(thalachh))
+    
+    user_input=[float(age),float(sex),float(cp),float(trtbps),float(chol),float(fbs),float(restecg),float(thalachh),float(exng),float(oldpeak),float(slp),float(caa),float(thallP)]
+    print('user_input ', user_input)
     user_input=np.array(user_input)
     user_input=user_input.reshape(1,-1)
-    #user_input=scal.fit_transform(user_input)
+    print('user_input reshaped ', user_input, ' ', type(user_input))
+    
     prediction = model.predict(user_input)
-   
+    print(prediction)
     return  prediction
        
     # front end elements of the web page 
@@ -90,27 +90,26 @@ st.markdown(html_temp, unsafe_allow_html = True)
       
 # following lines create boxes in which user can enter data required to make prediction
 age=st.selectbox ("Age",range(1,121,1))
-sex = st.radio("Select Gender: ", ('male', 'female'))
-cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
+sexP = st.radio("Select Gender: ", ('male', 'female'))
+cpP = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic")) 
 trtbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
 chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
-fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
-restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
+fbsP=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
+restecgP=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
 thalachh=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
-exng=st.selectbox('Exercise Induced Angina',["Yes","No"])
+exngP=st.selectbox('Exercise Induced Angina',["Yes","No"])
 oldpeak=st.number_input('Oldpeak')
-slp = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with excercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
+slpP = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with excercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
 caa=st.selectbox('Number of Major Vessels Colored by Flourosopy',range(0,4,1))
-thall=st.selectbox('Thalium Stress Result',range(0,4,1))
+thallP=st.selectbox('Thalium Stress Result',range(0,4,1))
 
-
-
-pred=preprocess(age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall)
+pred = preprocess(age,sexP,cpP,trtbps,chol,fbsP,restecgP,thalachh,exngP,oldpeak,slpP,caa,thallP)
 
 st.write(pred)
 
 
-if st.button("Predict"):    
+if st.button("Predict"):
+  
   if pred[0] == 1:
     st.error('Warning! You have high risk of getting a heart attack!')
     
